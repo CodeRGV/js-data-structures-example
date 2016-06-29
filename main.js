8a51190e887e66209166bcc6a0e22777caa1cdbf
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-06-29 16:22:27 -0500

    linked list example

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ list.add(1);
 list.add(2);
 list.add(3);
 
-list.forEach(function(item){
-  console.log(item);
-});
+list.forEach(console.log);
+
+list.add(5, 1);
+
+console.log('');
+list.forEach(console.log);
+
+list.del(0);
+
+console.log('');
+list.forEach(console.log);
+
+var node = list.getNode(2);
+
+console.log('');
+console.log(node);
